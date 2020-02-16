039a61032499f94e1c8cf0222aa691223fde6588
dhl0917 <dhl0917@live.com>
2020-02-15 19:29:45 -0500

    Build the foundation to connect all players. #21

--- project3/player.cpp
+++ project3/player.cpp
@@ -8,18 +8,17 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+  if(argc != 3){
+    cout << "Please check the number of parameters."<<endl;
+    return 1;
+  }
   int status;
   int socket_fd;
   struct addrinfo host_info;
   struct addrinfo *host_info_list;
   const char *hostname = argv[1];
-  const char *port     = "4444";
+  const char *port     = argv[2];
   
-  if (argc < 2) {
-      cout << "Syntax: client <hostname>\n" << endl;
-      return 1;
-  }
-
   memset(&host_info, 0, sizeof(host_info));
   host_info.ai_family   = AF_UNSPEC;
   host_info.ai_socktype = SOCK_STREAM;
@@ -34,13 +33,14 @@ int main(int argc, char *argv[])
   socket_fd = socket(host_info_list->ai_family, 
 		     host_info_list->ai_socktype, 
 		     host_info_list->ai_protocol);
+
   if (socket_fd == -1) {
     cerr << "Error: cannot create socket" << endl;
     cerr << "  (" << hostname << "," << port << ")" << endl;
     return -1;
   } //if
   
-  cout << "Connecting to " << hostname << " on port " << port << "..." << endl;
+  //  cout << "Connecting to " << hostname << " on port " << port << "..." << endl;
   
   status = connect(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
   if (status == -1) {
@@ -49,11 +49,22 @@ int main(int argc, char *argv[])
     return -1;
   } //if
 
-  const char *message = "hi there!";
-  send(socket_fd, message, strlen(message), 0);
+  int connectedFlag = 1;
+  send(socket_fd, &connectedFlag, sizeof(connectedFlag), 0);
 
+  int numberMessage[2];
+  recv(socket_fd,numberMessage,sizeof(numberMessage),0);
+  int id = numberMessage[0];
+  int num_players = numberMessage[1];
+  cout<<"Connected as player "<<id<<" out of "<<num_players<<" total players"<<endl;
+  
+  int closedFlag=0;
+  while(closedFlag==0){
+    recv(socket_fd,&closedFlag,sizeof(closedFlag),0);
+  }
+  
   freeaddrinfo(host_info_list);
   close(socket_fd);
-
+  cout<<"Player "<<id<<" closed"<<endl;
   return 0;
 }
--- project3/ringmaster.cpp
+++ project3/ringmaster.cpp
@@ -3,17 +3,28 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+   if(argc != 4){
+    cout << "Please check the number of parameters."<<endl;
+    return 1;
+  }
   int status;
   int socket_fd;
   struct addrinfo host_info;
   struct addrinfo *host_info_list;
   const char *hostname = NULL;
-  const char *port     = "4444";
+  const char *port     = argv[1];
+  int num_players = atoi(argv[2]);
+  int num_hops = atoi(argv[3]);
+
+  cout<<"Potato Ringmaster"<<endl;
+  cout<<"Players = "<<num_players<<endl;
+  cout<<"Hops = "<<num_hops<<endl;
 
   memset(&host_info, 0, sizeof(host_info));
 
@@ -54,21 +65,49 @@ int main(int argc, char *argv[])
   } //if
 
   cout << "Waiting for connection on port " << port << endl;
-  struct sockaddr_storage socket_addr;
-  socklen_t socket_addr_len = sizeof(socket_addr);
-  int client_connection_fd;
-  client_connection_fd = accept(socket_fd, (struct sockaddr *)&socket_addr, &socket_addr_len);
-  if (client_connection_fd == -1) {
-    cerr << "Error: cannot accept connection on socket" << endl;
-    return -1;
-  } //if
 
-  char buffer[512];
-  recv(client_connection_fd, buffer, 9, 0);
-  buffer[9] = 0;
+  // Three lists to store information for each player
+  int socket_array[num_players];
+  struct sockaddr_storage socket_addr_list[num_players];
+  socklen_t socket_addr_len_list[num_players];
 
-  cout << "Server received: " << buffer << endl;
+  int playerCounter = 0;
+  while(playerCounter<num_players){
+    struct sockaddr_storage socket_addr;
+    socklen_t socket_addr_len = sizeof(socket_addr);
+    socket_array[playerCounter] = accept(socket_fd, (struct sockaddr *)&socket_addr, &socket_addr_len);
+    if (socket_array[playerCounter] == -1) {
+      cerr << "Error: cannot accept connection on the socket from player "<<playerCounter<< endl;
+      return -1;
+    } //if
+    socket_addr_list[playerCounter] = socket_addr;
+    socket_addr_len_list[playerCounter] = socket_addr_len;
+    int connectedFlag=0;
+    recv(socket_array[playerCounter],&connectedFlag,sizeof(connectedFlag),0);
+    if(connectedFlag==1){
+      cout<<"Player "<<playerCounter<<" is ready to play"<<endl;
+      int numberMessage[2];
+      numberMessage[0] = playerCounter;
+      numberMessage[1] = num_players;
+      send(socket_array[playerCounter],&numberMessage,sizeof(numberMessage),0);
+    }
+    else{
+      cerr<<"Player "<<playerCounter<<" is down"<<endl;
+      return -1;
+    }
+    
+    playerCounter += 1;
+  }
+  
 
+  
+  
+
+  for(int i=0;i<num_players;i++){
+    int closedFlag=1;
+    send(socket_array[i], &closedFlag, sizeof(closedFlag), 0);
+  }
+  
   freeaddrinfo(host_info_list);
   close(socket_fd);
 
