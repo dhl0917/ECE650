0989a14686279207d7db558c3004fdbc9b70fd3c
dhl0917 <dhl0917@live.com>
2020-02-16 19:36:25 -0500

    To debug connection problem between different machines. #20

--- project3/player.cpp
+++ project3/player.cpp
@@ -68,6 +68,7 @@ int main(int argc, char *argv[])
   while(!syn){
     int signal;
     recv(socket_server,&signal,sizeof(signal),0); // Standby, waiting for orders from the server
+    cout<<signal<<endl;
     if(signal == 0){
       // All players connected and synchronized, break the loop
       syn = true;
@@ -117,6 +118,7 @@ int main(int argc, char *argv[])
       struct sockaddr_storage socket_addr_left;
       socklen_t socket_addr_len_left = sizeof(socket_addr_left);
       socket_left = accept(socket_listen, (struct sockaddr *)&socket_addr_left, &socket_addr_len_left);
+      cout<<"accepting"<<endl;
       if (socket_left == -1) {
         cerr << "Error: cannot accept connection on the socket of player "<<id<< endl;
         return -1;
@@ -170,10 +172,13 @@ int main(int argc, char *argv[])
         return -1;
       } //if
 
+      cout<<"i'm here 1"<<endl;
       status = -1;
       while(status==-1){
-	status = connect(socket_right, host_info_list->ai_addr, host_info_list->ai_addrlen);
+	      status = connect(socket_right, host_info_list->ai_addr, host_info_list->ai_addrlen);
       }
+      cout<<"i'm here 2"<<endl;
+
       connectedFlag = 1;
       send(socket_right, &connectedFlag, sizeof(connectedFlag), 0);
     }
